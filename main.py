1bce17a2e30c0364336247027f4f55920f281680
2451873434 <2451873434@qq.com>
2020-12-28 19:31:10 +0800

    彭敏

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import cv2 as cv
-import numpy as np
-import matplotlib as plt
 
-print('hallo')
+face_cascade = cv.CascadeClassifier(r'D:\python\LUCA\facebook\haarcascade_frontalface_default.xml')
+# 打开摄像机
 camera = cv.VideoCapture(0)
-cv.namedWindow("win")
+# 创建窗口
+cv.namedWindow('window')
+# 拍一张照片
+success, img = camera.read()    # success：拍摄是否成功    img：拍摄的图片
+if success:
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # 图片转成灰色
+    face = face_cascade.detectMultiScale(img_gray, 1.1, 3)  # 识别人脸
+    for (x, y, w, h) in face:
+        cv.rectangle(img, (10, 10), (110, 110), (0, 0, 255), 2) # 框出人脸
+cv.imshow('window', img)    # 显示框出人脸的图片
+cv.waitKey(0)
+camera.release()
+cv.destroyAllWindows()
+
--- ui/window.py
+++ ui/window.py
@@ -1,2 +1,7 @@
+import sys
+import os
 
-#window class
+dirname, filename = os.path.split(os.path.abspath( __file__))
+os.path.realpath(__file__)
+print(dirname)
+print(filename)
